2d83b5e8f201df1e2ff70f3acae84f30ea457be6
Phaninder Pasupula <pasupulaphani@gmail.com>
2016-11-04 14:39:46 +0000

    refactor

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![CircleCI](https://circleci.com/gh/pearlshare/google-client.svg?style=svg)](https://circleci.com/gh/pearlshare/google-client) [![NSP Status](https://nodesecurity.io/orgs/pearlshare/projects/269cab39-1dd8-492f-8605-645be715dfe1/badge)](https://nodesecurity.io/orgs/pearlshare/projects/269cab39-1dd8-492f-8605-645be715dfe1)
+
 # google-client
--- test/geocode.js
+++ test/geocode.js
@@ -1,8 +1,15 @@
-var should = require("should");
-var geocode = require("../lib/geocode");
+require("should");
+var Client = require("../lib/client");
 
 describe("geocode", function () {
-  it("should contain ", function () {
-    
+
+  var googleClient;
+  before(function () {
+    googleClient = new Client("testKey", console);
+  });
+
+  it("should contain geocode", function () {
+    console.log(googleClient)
+    googleClient.geocode.should.be.function();
   });
 });
